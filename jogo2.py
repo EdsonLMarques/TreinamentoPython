62ef5efd781e1b565abd2392115e23028aaefc93
Edson_LMarques <edsonmarques@gea.inatel.br>
2021-08-30 21:19:20 -0300

    teste

--- jogo2.py
+++ jogo2.py
@@ -4,26 +4,36 @@ def jogar():
     print("*******bem vindo ao jogo da forca!*******")
     print("*****************************************")
 
-    palavra_secreta = 'banana'
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = 'maça'.upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
+
     print(letras_acertadas)
     while(not(enforcou) and not(acertou)):
-
         chute = input("qual letra? ")
-        chute = chute.strip()
-
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-
+        chute = chute.strip().upper()
+        if(chute.upper()  in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros = erros + 1
+        if (erros == 6):
+            enforcou = True
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
         print("jogando")
 
-    print("fim de jogo")
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu")
+    print("FIM DE JOGO")
 
 if (__name__ == "__main__"):
     jogar()
